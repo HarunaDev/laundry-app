76c574e0225552a5ec59bcb2057a664694c910e0
Alvin Haruna <harunaalvin69@gmail.com>
2026-09-25 22:20:14 +0100

    created orders and orders details page with utils functions to format data

---
+++ mobile/utils/date.ts
@@ -0,0 +1,13 @@
+export function formatOrderDate(dateString: string): string {
+  const date = new Date(dateString);
+
+  if (Number.isNaN(date.getTime())) {
+    return "Unknown date";
+  }
+
+  return new Intl.DateTimeFormat("en-NG", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  }).format(date);
+}
